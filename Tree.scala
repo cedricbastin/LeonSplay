0ffdd3305d7760ddef5b1ff0ad13a7a05a4abc8e
cedricbastin <cedricbastin@gmail.com>
2015-04-09 19:58:24 +0200

    mid

--- Tree.scala
+++ Tree.scala
@@ -4,135 +4,192 @@ import leon.annotation._
 /**
  * { require(this.isPackedSet)
  * } ensuring { res => ... }
-**/
+ **/
+
+object PackedSet {
+  case class Range(min: Int, max: Int) {
+    def size: Int = {
+      require(min < max)
+      if (max >= min) max-min+1
+      else 0
+    }
+  }
+}
+
 
 object SplayTree {
-  abstract class Tree {
-    
-    // def maxSize(n:Int):Boolean = this match {
-    //   case Leaf => true
-    //   case Node(r, v, l) => (n > 0) || (l.maxSize(n-1) && r.maxSize(n-1))
-    // }
-    
-    def contains(v: Int):Boolean = {
-      require(isBinSearchTree(Int.MinValue, Int.MaxValue))
-      this match {
-        case Leaf => false
-        case Node(l, x, r) =>
-          if (v == x) true
-          else if (v < x) l.contains(v)
-          else r.contains(v)
-      } ensuring(res.isBinSearchTree(Int.MinValue, Int.MaxValue))
-    }
-    
-    def isBinSearchTree(min:Int, max:Int):Boolean = this match {
-      case Leaf => true
-      case Node(l, v, r) =>
-        (v > min) && (v < max) && l.isBinSearchTree(min, v) && r.isBinSearchTree(v, max)
-    }
-    
-    // def add(x:Int):Tree = this.addBinary(x).splay(x)
-    // def addBinary(x:Int):Tree = this match {
-    //   case Leaf => Node(Leaf, x, Leaf)
-    //   case n@Node(l, v, r) if (x == v) => n
-    //   case Node(l, v, r) if (x < v) => Node(l.addBinary(x), v, r)
-    //   case Node(l, v, r) if (x > v) => Node(l, v, r.addBinary(x))
-    // } //ensuring { res => res.isBinSearchTree() }
-    
-    // def remove(x:Int):Tree = {
-    //   this
-    // }
-    
-    // def join(that:Tree):Tree = {
-    //   this
-    // }
-    
-    // def split(x:Int):(Tree, Tree) = {
-    //   require(contains(x))
-    //   splay(x) match {
-    //     case Leaf => (Leaf, Leaf) //should never happen
-    //     case Node(l, v, r) if (v == x) => (Node(l, v, Leaf), r)
-    //     case Node(l, v, r) => (l, Node(Leaf, v, r)) //if splay returned parent
-    //   }
-    // }
-    
-    def splay(v:Int):Tree = {
-      //require(contains(v) && isBinSearchTree(Int.MinValue, Int.MaxValue))
-      this match {
-        case Leaf => Leaf //nothing to splay
-        case n @ Node(l, x, r) =>
-          if (v == x) n
-          else if (v < x) {
-            l match {
-              case Leaf => Node(l, x, r) //there is nothing smaller -> return parent
-              case Node(ll, lx, lr) =>
-                if (v == lx) Node(ll, v, Node(lr, x, r))
-                else if (v < lx) {
-                  ll match {
-                    case Leaf => Node(ll, lx, Node(lr, x, r))
-                    case _ => ll.splay(v) match {
-                      case Node(lll, llx, llr) => Node(lll, llx, Node(llr, lx, Node(lr, x, r)))
-                      //case else?
-                    }
-                  }
-                }
-                else {
-                  lr match {
-                    case Leaf => Node(ll, lx, Node(lr, x, r))
-                    case _ => lr.splay(v) match {
-                      case Node(lrl, lrx, lrr) => Node(Node(ll, lx, lrl), lrx, Node(lrr, x, r))
-                      //case else?
-                    }
-                  }
-                }
-            }
-          } else {
-            r match {
-              case Leaf => Node(l, x, r)
-              case Node(rl, rx, rr) => 
-                if (v == rx) {
-                  Node(Node(l,x,rl), v, rr)
-                } else if (v < rx) {
-                  rl match {
-                    case Leaf => Node(Node(l, x, rl), rx, rr)
-                    case _ => rl.splay(v) match {
-                      case Node(rll, rlx, rlr) => Node(Node(l, x, rll), rlx, Node(rlr, rx, rr))
-                      //case else? 
-                    }
-                  }
-                } else {
-                  rr match {
-                    case Leaf => Node(Node(l,x,rl),rx,rr)
-                    case _ => rr.splay(v) match {
-                      case Node(rrl, rrx, rrr) => Node(Node(Node(l, x, rl), rx, rrl), rrx, rrr)
-                      //case else?
-                    }
-                  }
-                }
-            }
-          }
-            
-      }
-    }
-    
-    // def splayMax:Tree = { //tree with empty left child
-    //   this match {
-    //     case Leaf => Leaf //nothing to splay?
-    //     case Node(l, v, Leaf) => this //max found
-    //     case Node(l, v, r) =>
-    //       rn = r.splayMax
-    //   }
-    // } require (_.l == Leaf)
-
-    // def getMinAndRemove:(Int, Tree) = {
-    //   require(this match {case n:Node => true case Leaf => false}) //can't remove anything if Leaf
-    //   this match { //or splayMin
-    //     case Node(Leaf, v, r) => (v, r)
-    //     case Node(l, v, r) => l.getMinAndRemove match {case (vn, tn) => (vn, Node(tn, v, r))}
-    //     case Leaf => (-1, this)
-    //   }
-    // }
+  sealed abstract class OptInt {
+    def <(a:Int):Boolean
+    def >(a:Int):Boolean
   }
+  case class Some(i:Int) extends OptInt {
+    def <(a:Int):Boolean = {i < a}
+    def >(a:Int):Boolean = {i > a}
+  }
+  case object None extends OptInt {
+    def <(a:Int):Boolean = {true}
+    def >(a:Int):Boolean = {true}
+  }
+
+  abstract class Tree
   case class Node(l:Tree, v:Int, r:Tree) extends Tree
   case object Leaf extends Tree
+
+  //to limit the size of the trees to examine
+  def maxSize(tree:Tree, n:Int):Boolean = tree match {
+    case Leaf => true
+    case Node(r, v, l) => (n > 0) && (maxSize(l, n-1) && maxSize(r, n-1))
+  }
+
+  def contains(tree:Tree, v: Int):Boolean = {
+    require(isSorted(tree)) //work for range 1 -> 10
+    tree match {
+      case Leaf => false
+      case Node(l, x, r) =>
+        if (v == x) true
+        else if (v < x) contains(l, v)
+        else contains(r, v)
+    }
+  }
+
+  //current impl
+  def isSorted(tree:Tree):Boolean = isSortedAcc(tree, true, None, None)
+
+  //4.tail recursive
+  def isSortedAcc(tree:Tree, bFlag:Boolean, min:OptInt, max:OptInt):Boolean = tree match {
+    case Leaf => true
+    case Node(l, v, r) =>
+      if (!bFlag) false
+      else isSortedAcc(r, isSortedAcc(l, (min < v && max > v), min, Some(v)), Some(v), max)
+  }
+
+  // 3.Optional boundaries better impl 
+  def isSortedOB2(tree:Tree, min:OptInt, max:OptInt):Boolean = tree match {
+    case Leaf => true
+    case Node(l, v, r) =>
+      if (!(min < v)) false
+      else if (!(max > v)) false
+      else if (!isSortedOB2(l, min, Some(v))) false
+      else isSortedOB2(r, Some(v), max)
+  }
+
+  // 2.Optional boundaries  
+  def isSortedOB(tree:Tree, min:OptInt, max:OptInt):Boolean = tree match {
+    case Leaf => true
+    case Node(l, vi, r) =>
+      (min, max) match {
+        case (None, None) => val v = Some(vi); isSortedOB(l, min, v) && isSortedOB(r, v, max)
+        case (Some(mi), None) => val v = Some(vi); (vi > mi) && isSortedOB(l, min, v) && isSortedOB(r, v, max)
+        case (None, Some(ma)) => val v = Some(vi); (vi < ma) && isSortedOB(l, min, v) && isSortedOB(r, v, max)
+        case (Some(mi), Some(ma)) => val v = Some(vi); (vi > mi) && (vi < ma) && isSortedOB(l, min, v) && isSortedOB(r, v, max)
+      }
+  }
+
+  // 1.First Algo
+  def isSortedBasic(tree:Tree,min:Int, max:Int):Boolean = tree match {
+    case Leaf => true
+    case Node(l, v, r) =>
+      (v > min) && (v < max) && isSortedBasic(l, min, v) && isSortedBasic(r, v, max)
+  }
+
+  // def add(x:Int):Tree = this.addBinary(x).splay(x)
+  // def addBinary(x:Int):Tree = this match {
+  //   case Leaf => Node(Leaf, x, Leaf)
+  //   case n@Node(l, v, r) if (x == v) => n
+  //   case Node(l, v, r) if (x < v) => Node(l.addBinary(x), v, r)
+  //   case Node(l, v, r) if (x > v) => Node(l, v, r.addBinary(x))
+  // } //ensuring { res => res.isBinSearchTree() }
+
+  // def remove(x:Int):Tree = {
+  //   this
+  // }
+
+  // def join(that:Tree):Tree = {
+  //   this
+  // }
+
+  // def split(x:Int):(Tree, Tree) = {
+  //   require(contains(x))
+  //   splay(x) match {
+  //     case Leaf => (Leaf, Leaf) //should never happen
+  //     case Node(l, v, r) if (v == x) => (Node(l, v, Leaf), r)
+  //     case Node(l, v, r) => (l, Node(Leaf, v, r)) //if splay returned parent
+  //   }
+  // }
+
+  def splay(tree:Tree, v:Int):Tree = {
+    require(maxSize(tree, 5) )//&& isSorted(tree, Int.MinValue, Int.MaxValue))
+    tree match {
+      case Leaf => Leaf //nothing to splay
+      case n @ Node(l, x, r) =>
+        if (v == x) n
+        else if (v < x) {
+          l match {
+            case Leaf => Node(l, x, r) //there is nothing smaller -> return parent
+            case Node(ll, lx, lr) =>
+              if (v == lx) Node(ll, v, Node(lr, x, r))
+              else if (v < lx) {
+                ll match {
+                  case Leaf => Node(ll, lx, Node(lr, x, r))
+                  case _ => splay(ll, v) match {
+                    case Node(lll, llx, llr) => Node(lll, llx, Node(llr, lx, Node(lr, x, r)))
+                    //case else?
+                  }
+                }
+              }
+              else {
+                lr match {
+                  case Leaf => Node(ll, lx, Node(lr, x, r))
+                  case _ => splay(lr, v) match {
+                    case Node(lrl, lrx, lrr) => Node(Node(ll, lx, lrl), lrx, Node(lrr, x, r))
+                    //case else?
+                  }
+                }
+              }
+          }
+        } else {
+          r match {
+            case Leaf => Node(l, x, r)
+            case Node(rl, rx, rr) =>
+              if (v == rx) {
+                Node(Node(l,x,rl), v, rr)
+              } else if (v < rx) {
+                rl match {
+                  case Leaf => Node(Node(l, x, rl), rx, rr)
+                  case _ => splay(rl, v) match {
+                    case Node(rll, rlx, rlr) => Node(Node(l, x, rll), rlx, Node(rlr, rx, rr))
+                    //case else?
+                  }
+                }
+              } else {
+                rr match {
+                  case Leaf => Node(Node(l,x,rl),rx,rr)
+                  case _ => splay(rr, v) match {
+                    case Node(rrl, rrx, rrr) => Node(Node(Node(l, x, rl), rx, rrl), rrx, rrr)
+                    //case else?
+                  }
+                }
+              }
+          }
+        }
+
+    }
+  } //ensuring {res => isSorted(res, Int.MinValue, Int.MaxValue)}
+
+  def zig = {
+    require (true)
+  }
+
+  def zigzig = {}
+
+  def zigzag = {}
+
+  // def getMinAndRemove:(Int, Tree) = {
+  //   require(this match {case n:Node => true case Leaf => false}) //can't remove anything if Leaf
+  //   this match { //or splayMin
+  //     case Node(Leaf, v, r) => (v, r)
+  //     case Node(l, v, r) => l.getMinAndRemove match {case (vn, tn) => (vn, Node(tn, v, r))}
+  //     case Leaf => (-1, this)
+  //   }
+  // }
 }
